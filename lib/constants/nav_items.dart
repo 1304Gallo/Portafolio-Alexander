97bab4746f6e4a4c3495c8aecd4f13d376006f00
1304Gallo <alex980413@gmail.com>
2025-09-30 09:49:25 -0700

    30-9-25

--- lib/constants/nav_items.dart
+++ lib/constants/nav_items.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
 List<String> navTitles = ["Home", "Skills", "Projects", "Contacts"];
+
+List<IconData> navIcons = [
+  Icons.home,
+  Icons.android_outlined,
+  Icons.back_hand,
+  Icons.quick_contacts_mail,
+];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:portafolio/constants/colors.dart';
 import 'package:portafolio/constants/nav_items.dart';
-import 'package:portafolio/styles/styles.dart';
-import 'package:portafolio/widgets/header_desktop.dart';
 import 'package:portafolio/widgets/header_mobile.dart';
-import 'package:portafolio/widgets/side_logo.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,23 +11,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final scaffoldkey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldkey,
       backgroundColor: CustomColor.scaffoldBg,
+      endDrawer: Drawer(
+        backgroundColor: CustomColor.scaffoldBg,
+        child: ListView(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                child: IconButton(onPressed: () {}, icon: Icon(Icons.close)),
+              ),
+            ),
+            for (int i = 0; i < navIcons.length; i++)
+              ListTile(
+                contentPadding: const EdgeInsets.symmetric(horizontal: 30),
+                titleTextStyle: const TextStyle(
+                  color: CustomColor.whitePrimary,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
+                ),
+                onTap: () {},
+                leading: Icon(navIcons[i]),
+                title: Text(navTitles[i]),
+              ),
+          ],
+        ),
+      ),
       body: ListView(
         scrollDirection: Axis.vertical,
         children: [
           //Main
           // HeaderDesktop(),
-          HeaderMobile(onMenuTap: () {}),
+          HeaderMobile(
+            onLogoTap: () {},
+            onMenuTap: () {
+              scaffoldkey.currentState?.openEndDrawer();
+              // Scaffold.of(context).openEndDrawer();
+            },
+          ),
           //Skills
           Container(height: 500, width: double.maxFinite),
           //PROJECTS
           Container(
             height: 500,
             width: double.maxFinite,
-            color: Colors.blueGrey,
+            color: const Color.fromARGB(255, 37, 146, 201),
           ),
           //CONTACTS
           Container(height: 500, width: double.maxFinite),
